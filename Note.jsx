88a2260ca9b0ae793086aa5208ae38e1d6c5266c
vikass99 <77562270+vikass99@users.noreply.github.com>
2021-06-06 18:27:56 +0530

    initial commit

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an Amazing bootcamp taken up by Shaurya Sinha We covered
+        everything from Scratch including javaScript, react.js,Html.
+      </p>
+    </div>
+  );
+}
+export default Note;
